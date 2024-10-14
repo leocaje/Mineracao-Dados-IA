c6fa12961ea43373f3a9cd5768f0254e98043033
LeoCaje <leoacaje@gmail.com>
2024-10-14 09:58:50 -0300

    first commit

---
+++ chat.py
@@ -0,0 +1,27 @@
+import os
+from openai import AzureOpenAI
+
+endpoint = os.getenv("ENDPOINT_URL", "https://leona-m28xpvmq-australiaeast.openai.azure.com/")
+deployment = os.getenv("DEPLOYMENT_NAME", "gpt-4-32k")
+subscription_key = os.getenv("CHAVE", "b6b5510d3f63461d84724cbaa1f2bb94")
+
+# Initialize Azure OpenAI client with key-based authentication
+client = AzureOpenAI(
+    azure_endpoint = endpoint,
+    api_key = subscription_key,
+    api_version = "2024-05-01-preview",
+)
+
+completion = client.chat.completions.create(
+    model=deployment,
+    messages= [{"role": "user", "content": "Produtos do Microsoft Office"}],
+    max_tokens=800,
+    temperature=0.7,
+    top_p=0.95,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop=None,
+    stream=False
+)
+
+print(completion.to_json())
